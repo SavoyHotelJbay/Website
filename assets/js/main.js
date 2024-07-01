25a801daa20d128b5a5f5e3dddd36ddd1fb7524f
SavoyHotelJbay <bookings@savoyhoteljbay.co.za>
2024-07-01 12:56:00 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+/*
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      ( '1281px',  '1680px' ),
+			normal:    ( '981px',   '1280px' ),
+			narrow:    ( '737px',   '980px'  ),
+			narrower:  ( '737px',   '840px'  ),
+			mobile:    ( '481px',   '736px'  ),
+			mobilep:   ( null,      '480px'  )
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			alignment: 'right'
+		});
+
+	// NavPanel.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+	// Header.
+		if (!browser.mobile
+		&&	$header.hasClass('alt')
+		&&	$banner.length > 0) {
+
+			$window.on('load', function() {
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight(),
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt reveal'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			});
+
+		}
+
+})(jQuery);
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const carousels = document.querySelectorAll('.carousel-container');
+    carousels.forEach(carouselContainer => {
+      const reel = carouselContainer.querySelector('.reel');
+      const slides = Array.from(reel.children);
+      const nextButton = carouselContainer.querySelector('.forward');
+      const prevButton = carouselContainer.querySelector('.backward');
+
+      let currentIndex = 0;
+
+      function updateCarousel() {
+        const offset = -currentIndex * 100;
+        reel.style.transform = `translateX(${offset}%)`;
+      }
+
+      nextButton.addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateCarousel();
+      });
+
+      prevButton.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        updateCarousel();
+      });
+
+      updateCarousel(); // Initial call to set the correct position
+    });
+  });
+
+
+  
